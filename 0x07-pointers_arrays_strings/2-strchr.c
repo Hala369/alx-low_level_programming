4b70420ba6f9a14ffc73954e5889ceec7b449cee
Hala369 <halamastaki@gmail.com>
2023-04-04 13:54:06 -0700

    0x07. C pointers, arrays and strings

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strchr - Entry point
+ *
+ * @s: input
+ *
+ * @c: input
+ *
+ * Return: a pointer to the first occurrence of the character
+ */
+
+char *_strchr(char *s, char c)
+{
+	while (*s)
+	{
+		if (*s != c)
+			s++;
+		else
+			return (s);
+	}
+	if (c == '\0')
+		return (s);
+
+	return (NULL);
+}
