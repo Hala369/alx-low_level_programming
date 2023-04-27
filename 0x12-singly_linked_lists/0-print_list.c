a6fee33d68e1b2b28f8ff9cb868b0d8b50bec71a
Hala369 <halamastaki@gmail.com>
2023-04-27 14:38:58 -0700

    Print list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -8,7 +8,7 @@
 */
 size_t print_list(const list_t *h)
 {
-	size_t d = 0;
+	size_t o = 0;
 
 	while (h)
 	{
@@ -17,7 +17,7 @@ size_t print_list(const list_t *h)
 		else
 			printf("[%u] %s\n", h->len, h->str);
 		h = h->next;
-		d++;
+		o++;
 	}
-	return (d);
+	return (o);
 }
