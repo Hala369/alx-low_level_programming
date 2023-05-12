1be4f51656a03ef9061bc6fbd079a31c5787c710
Hala369 <halamastaki@gmail.com>
2023-05-12 13:37:38 -0700

    cp

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -11,7 +11,7 @@
  */
 int main(int argc, char *argv[])
 {
-	int desc_r;
+	int desc_x;
 	int desc_w;
 	int x;
 	int y;
@@ -23,19 +23,19 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-	desc_r = open(argv[1], O_RDONLY);
-	if (desc_r < 0)
+	desc_x = open(argv[1], O_RDONLY);
+	if (desc_x < 0)
 	{
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
 	desc_w = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
-	while ((x = read(desc_r, buffer, BUFSIZ)) > 0)
+	while ((x = read(desc_x, buffer, BUFSIZ)) > 0)
 	{
 		if (desc_w < 0 || write(desc_w, buffer, x) != x)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
-			close(desc_r);
+			close(desc_x);
 			exit(99);
 		}
 	}
@@ -44,12 +44,12 @@ int main(int argc, char *argv[])
 		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		exit(98);
 	}
-	y = close(desc_r);
+	y = close(desc_x);
 	z = close(desc_w);
 	if (y < 0 || z < 0)
 	{
 		if (y < 0)
-			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", desc_r);
+			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", desc_x);
 		if (z < 0)
 			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", desc_w);
 		exit(100);
