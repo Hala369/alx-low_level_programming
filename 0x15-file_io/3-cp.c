b3ee47573a5e3ba81d9aabc6fd7118eff3e828f9
Hala369 <halamastaki@gmail.com>
2023-05-12 13:31:56 -0700

    cp

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,58 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - copies the content of a file to another file
+ * @argc: The number of arguments supplied to the program.
+ * @argv: An array of pointers to the arguments.
+ *
+ * Return: 0 on success.
+ */
+int main(int argc, char *argv[])
+{
+	int desc_r;
+	int desc_w;
+	int x;
+	int y;
+	int z;
+	char buffer[BUFSIZ];
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
+		exit(97);
+	}
+	desc_r = open(argv[1], O_RDONLY);
+	if (desc_r < 0)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	desc_w = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	while ((x = read(desc_r, buffer, BUFSIZ)) > 0)
+	{
+		if (desc_w < 0 || write(desc_w, buffer, x) != x)
+		{
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+			close(desc_r);
+			exit(99);
+		}
+	}
+	if (x < 0)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	y = close(desc_r);
+	z = close(desc_w);
+	if (y < 0 || z < 0)
+	{
+		if (y < 0)
+			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", desc_r);
+		if (z < 0)
+			dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", desc_w);
+		exit(100);
+	}
+	return (0);
+}
